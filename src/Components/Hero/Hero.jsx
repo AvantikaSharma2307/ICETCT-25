84c9d2ffa75119cd76f5810b21b6508a21b5ca84
ujjwal <ujjwaltomar.0808@gmail.com>
2024-11-13 21:45:35 +0530

    made changes in navbar and hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,11 +6,15 @@ const Hero = () => {
         <>
         <div className="hero ">
             <div className="hero-text">
-                <h1>International Conference on Emerging Trends in Computational Techniques(ICETCT-2025)
+                <h1>International Conference on Emerging Trends in Computational Techniques
                 </h1>
+                <h1>(ICETCT-2025)</h1>
                 <h2>
-                On May 30-31, 2025 Organized by: Department of CSE-AI & Department of CSE-AIML
+                On May 30-31, 2025 
                 </h2>
+                <h2>Organized by: Department of CSE-AI </h2>
+                <h2>&</h2>
+                <h2> Department of CSE-AIML</h2>
             </div>
         </div>
         <h1 className="text-center text-3xl text-bold mt-14"> About ICETCT </h1>
